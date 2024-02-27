149a3771e54d6e3ab51fd3e008a0ffbf274b1f75
Mason Kulikowski <masonkul@udel.edu>
2024-02-27 12:05:55 -0500

    to markdown

--- src/objects.ts
+++ src/objects.ts
@@ -82,7 +82,13 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let options = "";
+    let markdown = `# ${question.name}\n${question.body}`;
+    if (question.type === "multiple_choice_question") {
+        options = question.options.map((option) => `- ${option}`).join("\n");
+        markdown += `\n${options}`;
+    }
+    return markdown;
 }
 
 /**
