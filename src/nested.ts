dee1f032bd16c05222c80cc62fb3e04fa51c5ddd
Mason Kulikowski <masonkul@udel.edu>
2024-02-27 13:46:56 -0500

    sum published points

--- src/nested.ts
+++ src/nested.ts
@@ -62,7 +62,9 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return questions.reduce((num, question) => {
+        return question.published ? num + question.points : num;
+    }, 0);
 }
 
 /***
