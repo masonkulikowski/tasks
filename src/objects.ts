cd64deff4a71e2f210142c8dad7179a492e63f8b
Mason Kulikowski <masonkul@udel.edu>
2024-02-27 11:56:34 -0500

    to short form

--- src/objects.ts
+++ src/objects.ts
@@ -61,7 +61,7 @@ export function isValid(
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return `${question.id}: ${question.name.slice(0, 10)}`;
 }
 
 /**
