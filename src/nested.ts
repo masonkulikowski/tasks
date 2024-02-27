dd5f8a1810d632edf239a3e857b363ca2d2bb19b
Mason Kulikowski <masonkul@udel.edu>
2024-02-27 14:00:09 -0500

    same type

--- src/nested.ts
+++ src/nested.ts
@@ -129,7 +129,7 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    return questions.every((question) => question.type === questions[0].type);
 }
 
 /***
