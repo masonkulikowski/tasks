681e6eb004d984627a2f49cdef63da51f2058b53
Mason Kulikowski <masonkul@udel.edu>
2024-02-27 12:06:49 -0500

    rename question

--- src/objects.ts
+++ src/objects.ts
@@ -96,6 +96,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
+    question.name = newName;
     return question;
 }
 
